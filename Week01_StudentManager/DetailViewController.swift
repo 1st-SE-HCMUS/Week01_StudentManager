1c2174353f7145a88317ea49f272f2c2d6868c49
hieutam <nhoxbypass@gmail.com>
2017-03-27 22:42:42 +0700

    support get NSDate from DatePicker

--- Week01_StudentManager/DetailViewController.swift
+++ Week01_StudentManager/DetailViewController.swift
@@ -70,6 +70,17 @@ class DetailViewController: UIViewController, UIPickerViewDelegate, UIPickerView
             let classIndex = (myStudent?.myClass.getIdNumber())! - 1
             classPicker.selectRow(classIndex, inComponent: 0, animated: true)
             myClass = myStudent!.myClass
+            
+            // Specify date components
+            var dateComponents = DateComponents()
+            dateComponents.year = myStudent?.dateOfBirth.year
+            dateComponents.month = myStudent?.dateOfBirth.month
+            dateComponents.day = myStudent?.dateOfBirth.day
+            
+            // Create date from components
+            let userCalendar = Calendar(identifier: .gregorian)
+            
+            datePicker.setDate(userCalendar.date(from: dateComponents)!, animated: true)
             btnSave.setTitle("Edit", for: .normal)
         }
         else
@@ -116,7 +127,12 @@ class DetailViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     }
     
     func saveDataAndDismiss() {
-        var date = MyDate()
+        let myCalendar = Calendar(identifier: .gregorian)
+        var birthDate = myCalendar.component(.day, from: datePicker.date)
+        var birthMonth = myCalendar.component(.month, from: datePicker.date)
+        var birthYear = myCalendar.component(.year, from: datePicker.date)
+        var date = MyDate(day: birthDate, month: birthMonth, year: birthYear)
+        
         myStudent = Student(firstName: edFirstNam.text!, lastName: edLastName.text!, dateOfBirth: date, myClass: myClass, otherInfo: edOtherInfo.text!)
         
         if delegate != nil {
